b755f0f20c85c0812f501498c1d435977c782f3d
David wanjohi <davidwagura83@gmail.com>
2024-02-29 18:22:12 +0300

    create web route

--- kinsta-auth/routes/web.php
+++ kinsta-auth/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/register', [RegisterController::class, 'create']);
+Route::post('/register', [RegisterController::class, 'store']);
